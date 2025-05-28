3fff4906d2209d0d22b6cffbc12fa028d6f1ebad
spm1102@ <dovietdungsl@gmail.com>
2025-05-28 18:17:46 +0700

    [MODIFY] fix bug in RTL of uart_rx and uart_rx_tb

--- dut/uart_rx.sv
+++ dut/uart_rx.sv
@@ -10,368 +10,144 @@ module uart_rx(
     input               parity_en,
     input               parity_type,
 
+    input               rts_n,
+
     output logic        rx_done,
-    output logic        rts_n,
     output logic        parity_error,
     output logic [7:0]  rx_data
 );
 
-typedef enum {IDLE, START, DATA, STOP} state_t;
+typedef enum logic [2:0] {IDLE, START, DATA, PARITY, STOP} state_t;
 state_t current_state, next_state;
 
 logic [3:0] num_data;
 logic [1:0] num_stop;
-
 logic [3:0] count_data;
 logic [1:0] count_stop;
-// logic [2:0] count_data_bit1;
-logic [3:0] tick_cnt;
+logic [4:0] tick_cnt;
+logic [7:0] rx_shift;
+logic parity_calc, parity_bit;
 
-logic parity_bit;
-logic [7:0] data_shift;
-
-// assign cts_n = rts_n; // cts_n = 1 khi ben kia chua san sang nhan
-
-// Mach com giai ma dau vao
+// Decode data bit number
 always_comb begin
     case (data_bit_num)
-        2'b00: num_data = 4'h5;
-        2'b01: num_data = 4'h6;
-        2'b10: num_data = 4'h7;
-        2'b11: num_data = 4'h8;
-        default: num_data = 4'h0;
+        2'b00: num_data = 5;
+        2'b01: num_data = 6;
+        2'b10: num_data = 7;
+        2'b11: num_data = 8;
+        default: num_data = 5;
     endcase
 end
 
+// Decode stop bit number
 always_comb begin
-    if (stop_bit_num) num_stop = 2;
-    else num_stop = 1;
+    num_stop = stop_bit_num ? 2 : 1;
 end
 
+// Next state logic
 always_comb begin
-    if (parity_type) // odd parity
-        parity_bit = ~^data_shift;
-    else             // even parity
-        parity_bit = ^data_shift;
-end
-
-// Mach com tinh toan trang thai tiep theo
-always_comb begin
+    next_state = current_state;
     case (current_state)
         IDLE: begin
-            if (~rx ) next_state = START;
-            // else next_state = IDLE;
+            if (~rx) next_state = START;
             else begin end
-            rts_n = 0;
         end
         START: begin
-            if (tick) begin
-                if (tick_cnt == 4'b0111) next_state = DATA;
-                else next_state = START;
-            end else next_state = START;
-            rts_n = 1;
+            if (tick && tick_cnt == 7)
+                next_state = DATA;
+            else begin end
         end
         DATA: begin
-            if (tick) begin
-                if (tick_cnt == 4'b1111) begin
-                    if (count_data == num_data + parity_en - 1) next_state = STOP;
-                    else next_state = DATA;
-                end else next_state = DATA;
-            end else next_state = DATA;
-            rts_n = 1;
+            if (tick && tick_cnt == 15 && count_data == num_data - 1)
+                next_state = (parity_en) ? PARITY : STOP;
+            else begin end
+        end
+        PARITY: begin
+            if (tick && tick_cnt == 15)
+                next_state = STOP;
+            else begin end
         end
         STOP: begin
-            if (tick) begin
-                if (tick_cnt == 4'b1111) begin
-                    if (count_stop == num_stop - 1) next_state = IDLE;
-                    else next_state = STOP;
-                end else next_state = STOP;
-            end else next_state = STOP;
-            rts_n = 1;
-        end
-        default: begin 
-            rts_n = 1;
-            next_state = IDLE;
+            if (tick && tick_cnt == 15 && count_stop == num_stop - 1)
+                next_state = IDLE;
+            else begin end
         end
     endcase
 end
 
-// Mach ff chuyen trang thai
+// State transition
 always_ff @(posedge clk or negedge rst_n) begin
-    if (~rst_n) current_state <= IDLE;
-    else if (tick) current_state <= next_state;
-    else begin end
+    if (!rst_n)
+        current_state <= IDLE;
+    else
+        current_state <= next_state;
 end
 
-// Mach ff tang bien dem va doc du lieu
+// Output and internal logic
 always_ff @(posedge clk or negedge rst_n) begin
-    if (~rst_n) begin
+    if (!rst_n) begin
+        tick_cnt <= 0;
         count_data <= 0;
         count_stop <= 0;
-        // count_data_bit1 <= 0;
-        tick_cnt <= 0;
-        data_shift <= 0;
-    end else if (tick) begin
-        if (tick_cnt == 4'b1111) begin
+        rx_shift <= 0;
+        parity_calc <= 0;
+        parity_bit <= 0;
+        parity_error <= 0;
+        rx_done <= 0;
+        rx_data <= 0;
+    end else begin
+        rx_done <= 0;
+        if (tick) begin
+            if (current_state != IDLE && tick_cnt == 15)
+                tick_cnt <= 0;
+            else if (current_state != IDLE)
+                tick_cnt <= tick_cnt + 1;
+
             case (current_state)
-                IDLE: begin
-                    count_data <= 0;
-                    count_stop <= 0;
-                    // count_data_bit1 <= 0;
-                    tick_cnt <= 0;
-                    data_shift <= 0;
-                end
                 START: begin
-                end
-                DATA: begin
-                    data_shift[count_data] <= rx;
-                    // if (rx) count_data_bit1 <= count_data_bit1 + 1;
-                    count_data <= count_data + 1;
-                    tick_cnt <= 0;
-                end
-                STOP: begin
-                    if (next_state == IDLE) begin
-                        count_stop <= 0;
-                        count_data <= 0;
-                        // count_data_bit1 <= 0;
+                    if (next_state == DATA) begin
                         tick_cnt <= 0;
                     end
-                    else begin
+                end
 
-                    count_stop <= count_stop + 1;
-                    count_data <= 0;
-                    // count_data_bit1 <= 0;
-                    tick_cnt <= 0;
+                DATA: begin
+                    if (tick_cnt == 15) begin
+                        rx_shift[count_data] <= rx;
+                        parity_calc <= parity_calc ^ rx;
+                        count_data <= count_data + 1;
                     end
                 end
-                default: begin
-                    count_stop <= 0;
-                    count_data <= 0;
-                    // count_data_bit1 <= 0;
-                    tick_cnt <= 0;
-                end
-            endcase
-        end
-        else  begin
-            case (current_state) 
-                START: begin
-                    if (next_state == DATA) tick_cnt <= 0;
-                    else tick_cnt <= tick_cnt + 1;
-                end
-                default: tick_cnt <= tick_cnt + 1;
-            endcase
-        end
-    end
-end
 
-// Mach ff dau ra
-always_ff @(posedge clk or negedge rst_n) begin
-    if (~rst_n) begin
-        rx_done <= 0;
-        parity_error <= 0;
-        rx_data <= 0;
-    end else if (tick) begin
-        case (current_state)
-            STOP: begin
-                if (next_state == IDLE) begin
-                    rx_done <= 1;
-                    rx_data <= data_shift;
-                    if (parity_en && (parity_bit != data_shift[num_data])) parity_error <= 1;
-                    else parity_error <= 0;
-                end else rx_done <= 0;
-            end
-            default: rx_done <= 0;
-        endcase
+                PARITY: begin
+                    if (tick_cnt == 15) begin
+                        parity_bit <= (parity_type) ? ~parity_calc : parity_calc;
+                        parity_error <= (rx != ((parity_type) ? ~parity_calc : parity_calc));
+                    end
+                end
+
+                STOP: begin
+                    if (tick_cnt == 15) begin
+                        count_stop <= count_stop + 1;
+                        if (count_stop == num_stop - 1) begin 
+                            rx_done <= 1;
+                            rx_data <= rx_shift;
+                        end
+                        else begin end
+                    end
+                    else begin end
+                end
+
+                IDLE: begin
+                    tick_cnt <= 0;
+                    count_data <= 0;
+                    count_stop <= 0;
+                    rx_shift <= 0;
+                    parity_calc <= 0;
+                    parity_error <= 0;
+                end
+            endcase
+        end
     end
 end
 
 endmodule
-
-// module uart_rx(
-//     input               clk,
-//     input               tick,
-//     input               rst_n,
-
-//     input               rx,
-
-//     input [1:0]         data_bit_num,
-//     input               stop_bit_num,
-//     input               parity_en,
-//     input               parity_type,
-
-//     input               rts_n,
-
-//     output logic        rx_done,
-//     output logic        cts_n,
-//     output logic        parity_error,
-//     output logic [7:0]  rx_data
-// );
-// typedef enum logic [2:0] {IDLE, START, DATA, PARITY, STOP, DONE} state_t;
-// state_t current_state, next_state;
-
-
-// logic [3:0] num_data;
-// logic [1:0] num_stop;
-
-// logic [3:0] count_data;
-// logic [1:0] count_stop;
-
-// logic [4:0] tick_cnt;
-// logic [7:0] rx_shift;
-// logic parity_calc;
-// logic parity_bit;
-
-// always_comb begin
-//     case (data_bit_num) 
-//     2'b00: num_data = 4'h5;
-//     2'b01: num_data = 4'h6;
-//     2'b10: num_data = 4'h7;
-//     2'b11: num_data = 4'h8;
-//     default: num_data = 4'h0;
-//     endcase
-// end
-// // Map number of stop bits
-// always_comb begin
-//     if (stop_bit_num) num_stop = 2;
-//     else num_stop = 1;
-// end
-
-// // FSM: next state logic
-// always_comb begin
-//     next_state = current_state;
-//     case (current_state)
-//         IDLE: begin
-//             if(~rx) next_state = START;
-//         end
-
-//         START: begin
-//             if (tick && tick_cnt == 7) //mid of start bit
-//                 next_state = DATA;
-//         end
-
-//         DATA: begin
-//             if(tick && tick_cnt == 15 && count_data == num_data)
-//                 next_state = (parity_en) ? PARITY : STOP;
-//         end
-
-//         PARITY: begin
-//             if( tick && tick_cnt == 15)
-//                 next_state = STOP;
-//         end
-
-//         STOP: begin
-//             if(tick && tick_cnt == 15 && count_stop == num_stop)
-//                 next_state = DONE;
-//         end
-
-//         DONE: begin
-//             next_state = IDLE;
-//         end
-//         default: begin
-//         end
-//     endcase
-// end
-
-// // FSM: state register
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) current_state <= IDLE;
-//     else current_state <= next_state;
-// end
-
-// //Tick counter
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if( !rst_n) tick_cnt <= 0;
-//     else if(tick) begin
-//         if(current_state == IDLE)
-//             tick_cnt <= 0;
-//         else if (current_state == START) begin
-//             if (next_state == DATA) tick_cnt <= 0;
-//             else tick_cnt <= tick_cnt + 1;
-//         end
-//         else if(tick_cnt == 15)
-//             tick_cnt <= 0;
-//         else
-//             tick_cnt <= tick_cnt + 1;
-//     end
-// end
-
-// //Data bit counter
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) count_data <= 0;
-//     else if (current_state == DATA && tick && tick_cnt == 15)
-//         count_data <= count_data + 1;
-//     else if (current_state == START)
-//         count_data <= 0;
-//     if(current_state == IDLE)
-//         count_data <= 0;
-    
-// end
-
-// //Stop bit counter
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) count_stop <= 0;
-//     else if (current_state == STOP && tick && tick_cnt == 15)
-//         count_stop <= count_stop + 1;
-//     else if (current_state == IDLE)
-//         count_stop <= 0;
-// end
-
-// // Shift register for RX data
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) rx_shift <= 0;
-//     else if (current_state == DATA && tick && tick_cnt == 15)
-//         if (next_state == STOP) begin end
-//         else rx_shift[count_data]  <= rx;
-//     if(current_state == IDLE) rx_shift <= 0;
-// end
-
-// //Parity calculation
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) parity_calc <= 0;
-//     else if(current_state == START)
-//         parity_calc <= 0;
-//     else if( current_state == DATA && tick && tick_cnt == 15)
-//         parity_calc <= parity_calc ^ rx;
-//     if (current_state == IDLE)parity_calc <= 0;
-// end
-
-// //Parity checking
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n)begin
-//          parity_error <= 0;
-//          parity_bit <= 0;
-//     end
-//     else if(current_state == PARITY && tick && tick_cnt == 15) begin
-//         if(parity_type)
-//             parity_bit <= ~parity_calc;
-//         else
-//             parity_bit <= parity_calc;
-
-//         parity_error <= (rx != parity_bit);
-//     end
-//     if (current_state == IDLE) begin
-//         parity_bit <= 0;
-//         parity_error <= 0;
-
-//     end
-// end
-
-// //Output assignment
-// always_ff @(posedge clk or negedge rst_n) begin
-//     if(!rst_n) begin
-//         rx_done <= 0;
-//         rx_data <= 0;
-//     end
-//     else begin
-//         if(current_state == DONE) begin
-//             rx_done <= 1;
-//             rx_data <= rx_shift;
-//         end else begin
-//             rx_done <= 0;
-//         end
-//     end
-// end
-
-// // CTS output
-// assign cts_n = 1'b0;
-// endmodule
--- tb/uart_rx_tb.sv
+++ tb/uart_rx_tb.sv
@@ -1,24 +1,21 @@
 `timescale 1ns/1ps
 
-module uart_rx_tb();
+module uart_rx_tb;
 
-    reg clk;
-    reg rst_n;
-    reg tick;
-    reg rx;
+    logic clk;
+    logic rst_n;
+    logic tick;
+    logic rx;
+    logic [1:0] data_bit_num;
+    logic stop_bit_num;
+    logic parity_en;
+    logic parity_type;
+    logic rts_n;
 
-    reg [1:0] data_bit_num;
-    reg stop_bit_num;
-    reg parity_en;
-    reg parity_type;
-    reg rts_n;
+    logic rx_done;
+    logic parity_error;
+    logic [7:0] rx_data;
 
-    wire rx_done;
-    wire cts_n;
-    wire parity_error;
-    wire [7:0] rx_data;
-
-    // Instance uart_rx
     uart_rx uut (
         .clk(clk),
         .tick(tick),
@@ -30,129 +27,148 @@ module uart_rx_tb();
         .parity_type(parity_type),
         .rts_n(rts_n),
         .rx_done(rx_done),
-        .cts_n(cts_n),
         .parity_error(parity_error),
         .rx_data(rx_data)
     );
 
-    // Clock generation 50 MHz
-    initial begin
-        clk = 0;
-        forever #10 clk = ~clk;
+    // Clock generation (100MHz)
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    // Tick generation (every 1040ns for 9600 baud with 16x oversampling)
+    initial tick = 0;
+    always begin
+        #1040 tick = 1;
+        #10 tick = 0;
     end
 
-    // Generate tick at baud rate ~9600 (every 326 clocks)
-    integer tick_counter = 0;
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            tick <= 0;
-            tick_counter <= 0;
-        end else begin
-            if (tick_counter == 163) begin
-                tick <= 1;
-                tick_counter <= 0;
-            end else begin
-                tick <= 0;
-                tick_counter <= tick_counter + 1;
-            end
-        end
-    end
+    // Task to send a UART frame
+    // UART frame sender task with proper parity calculation
+    task send_uart_frame(
+      input [7:0] data,
+      input [1:0] num_data_bits,
+      input num_stop_bits,
+      input parity_enable,
+      input parity_odd
+    );
+      integer i, actual_data_bits;
+      logic calc_parity;
+      begin
+        // Calculate actual number of data bits (5,6,7,8)
+        actual_data_bits = num_data_bits + 5;
+        
+        $display("Sending frame: data=0x%02h, data_bits=%0d, stop_bits=%0d, parity_en=%0b, parity_type=%s",
+                data, actual_data_bits, num_stop_bits ? 2 : 1, parity_enable, parity_odd ? "odd" : "even");
 
-    // Task giữ tín hiệu rx trong 16 tick
-    task uart_send_bit(input bit_val);
-        integer j;
-        begin
-            rx = bit_val;
-            for (j = 0; j < 16; j = j + 1) begin
-                @(posedge tick);
-            end
+        // Start bit
+        rx <= 0;
+        repeat (16) @(posedge tick);
+
+        // Data bits (LSB first) - only send the required number of bits
+        calc_parity = 0;
+        for (i = 0; i < actual_data_bits; i++) begin
+          rx <= data[i];
+          calc_parity = calc_parity ^ data[i]; // Calculate parity
+          repeat (16) @(posedge tick);
         end
+
+        // Parity bit (if enabled)
+        if (parity_enable) begin
+          if (parity_odd)
+            rx <= ~calc_parity; // Odd parity
+          else
+            rx <= calc_parity;   // Even parity
+          repeat (16) @(posedge tick);
+        end
+        wait(rx_done == 1);
+
+        // Stop bits
+        rx <= 1;
+        repeat (16) @(posedge tick);
+        
+        if (num_stop_bits) begin // 2 stop bits
+          rx <= 1;
+          repeat (16) @(posedge tick);
+        end
+
+        // Idle time between frames
+        repeat (16) @(posedge tick);
+      end
     endtask
-
-    // Task gửi 1 byte UART, hỗ trợ parity và stop bit
-    task uart_send_byte(input [7:0] data);
-        integer i;
-        reg parity_bit;
-        begin
-            // Start bit = 0
-            uart_send_bit(0);
-
-            // Data bits theo data_bit_num
-            parity_bit = 0;
-            for (i = 0; i < (data_bit_num + 5); i = i + 1) begin
-                uart_send_bit(data[i]);
-                parity_bit = parity_bit ^ data[i];
-            end
-
-            // Parity bit (nếu enable)
-            if (parity_en) begin
-                if (parity_type) // odd parity
-                    uart_send_bit(~parity_bit);
-                else            // even parity
-                    uart_send_bit(parity_bit);
-            end
-
-            // Stop bit(s)
-            uart_send_bit(1);
-            if (stop_bit_num)
-                uart_send_bit(1);
-        end
-    endtask
-
-    // Procedure test 1 case
+    // Test case task
     task test_case(
-        input [7:0] test_data,
-        input [1:0] test_data_bit_num,
-        input test_stop_bit_num,
-        input test_parity_en,
-        input test_parity_type
+      input [7:0] test_data,
+      input [1:0] test_data_bits,
+      input test_stop_bits,
+      input test_parity_en,
+      input test_parity_type,
+      input string test_name
     );
     begin
-        data_bit_num = test_data_bit_num;
-        stop_bit_num = test_stop_bit_num;
-        parity_en = test_parity_en;
-        parity_type = test_parity_type;
+      $display("\n=== %s ===", test_name);
+      
+      // Configure UART settings
+      data_bit_num <= test_data_bits;
+      stop_bit_num <= test_stop_bits;
+      parity_en <= test_parity_en;
+      parity_type <= test_parity_type;
+      
+      // Wait for settings to propagate
+      repeat (10) @(posedge clk);
+      
+      // Send UART frame
+      send_uart_frame(test_data, test_data_bits, test_stop_bits, test_parity_en, test_parity_type);
 
-        $display("\n--- Test case: data_bits=%0d, stop_bit=%0d, parity_en=%0b, parity_type=%0b ---",
-                 data_bit_num+5, stop_bit_num ? 2 : 1, parity_en, parity_type);
+      // Wait for reception to complete
+      //wait (rx_done == 1);
+      repeat (5) @(posedge clk);
 
-        uart_send_byte(test_data);
-
-        wait(rx_done);
-
-        #20; // chờ tín hiệu ổn định
-
-        $display("Sent byte: %h, Received byte: %h, Parity error: %b", test_data, rx_data, parity_error);
-
-        #100;
+      // Display results
+      $display("Expected: 0x%02h, Received: 0x%02h, Parity Error: %b, Match: %s", 
+              test_data, rx_data, parity_error, 
+              (rx_data == test_data && !parity_error) ? "PASS" : "FAIL");
+      
+      // Wait for rx_done to deassert
+      wait (rx_done == 0);
+      repeat (32) @(posedge tick);
     end
     endtask
-
     initial begin
-        // Initialize
-        rst_n = 0;
-        rx = 1; // idle line
-        rts_n = 1;
+    // Initialize signals
+        rx <= 1; // UART line idle high
+        rst_n <= 0;
+        rts_n <= 0; // Ready to receive
+        
+        // Initialize configuration to safe defaults
+        data_bit_num <= 2'b11; // 8 bits
+        stop_bit_num <= 1'b0;  // 1 stop bit
+        parity_en <= 0;        // No parity
+        parity_type <= 0;      // Even parity (when enabled)
 
-        #100;
-        rst_n = 1;
+        // Reset pulse
+        #200;
+        rst_n <= 1;
+        #200;
 
-        #100;
+        $display("Starting UART RX Tests...");
+        $display("Received: %h, Parity Error: %b", rx_data, parity_error);
+         // Test 1: Basic 8N1 (8 data bits, no parity, 1 stop bit)
+        test_case(8'hA5, 2'b11, 1'b0, 1'b0, 1'b0, "Test 1: 8N1 - 0xA5");
 
-        // Test 1: 8 data bits, 1 stop bit, no parity
-        test_case(8'hA5, 2'b11, 0, 0, 0);
+        // Test 2: 7E1 (7 data bits, even parity, 1 stop bit)
+        test_case(8'h55, 2'b10, 1'b0, 1'b1, 1'b0, "Test 2: 7E1 - 0x55");
 
-        // Test 2: 7 data bits, 1 stop bit, parity even
-        test_case(8'h55, 2'b10, 0, 1, 0);
+        // Test 3: 6O2 (6 data bits, odd parity, 2 stop bits)
+        test_case(8'h2A, 2'b01, 1'b1, 1'b1, 1'b1, "Test 3: 6O2 - 0x2A");
 
-        // Test 3: 6 data bits, 2 stop bits, parity odd
-        test_case(8'h2A, 2'b01, 1, 1, 1);
+        // Test 4: 5N2 (5 data bits, no parity, 2 stop bits)
+        test_case(8'h1B, 2'b00, 1'b1, 1'b0, 1'b0, "Test 4: 5N2 - 0x1B");
 
-        // Test 4: 5 data bits, 2 stop bits, no parity
-        test_case(8'h1B, 2'b00, 1, 0, 0);
+        // Test 5: 8E1 with different data patterns
+        test_case(8'h00, 2'b11, 1'b0, 1'b1, 1'b0, "Test 5: 8E1 - 0x00");
+        test_case(8'hFF, 2'b11, 1'b0, 1'b1, 1'b0, "Test 6: 8E1 - 0xFF");
 
-        #500;
-        #15000
+        #5000;
         $finish;
     end
 
